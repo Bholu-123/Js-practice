54d60ecbb27f60bcf673bbcaeb09a3a7174bdf15
Bholu-123 <67494074+Bholu-123@users.noreply.github.com>
2024-06-12 14:17:04 +0530

    Update Polyfill.js

--- Polyfill.js
+++ Polyfill.js
@@ -187,6 +187,60 @@ Promise.allPolyfill([
     console.log(err);
   });
 
+//Promise.any() Polyfill================================================================================================
+
+//Promise.any() takes an iterable of Promise objects which is promises in our polyfill.
+//It returns a single promise that fulfills as soon as any of the promises in the iterable fulfills, with the value of the fulfilled promise.
+//If no promises in the iterable fulfill (if all of the given promises are rejected), 
+//then the returned promise is rejected with an AggregateError, a new subclass of Error that groups together individual errors.
+
+function any(promises) {
+  let results = [];
+  var counter = 0;
+
+  return new Promise((resolve, reject) => {
+    promises.forEach((p, index) => {
+      p.then((result) => {
+        resolve(result)
+      }).catch((err) => {
+         results.push(err);
+         ++counter;
+        if (counter === promises.length) {
+          reject(results);
+        }
+      });
+    });
+  });
+};
+
+
+
+//Promise.allSettled Polyfill============================================================================================
+/*The Promise.allSettled() method returns a promise that fulfills after all of the given promises have either fulfilled or rejected,
+with an array of objects that each describes the outcome of each promise.
+It is used when you have multiple asynchronous tasks that are not dependent on one another to complete successfully, 
+  or you'd always like to know the result of each promise. */
+
+function allSettled(promises) {
+  let mappedPromises = promises.map((p) => {
+    return p.then((value) => {
+        return {
+          status: 'fulfilled',
+          value,
+        };
+      })
+      .catch((reason) => {
+        return {
+          status: 'rejected',
+          reason,
+        };
+      });
+  });
+  return Promise.all(mappedPromises);
+};
+
+
+
 //Call polyfill==============================================================================================================
 
 let car1 = {
